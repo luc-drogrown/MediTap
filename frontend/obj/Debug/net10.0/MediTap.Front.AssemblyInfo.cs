5b6bb2faf42374a370885c188e584b3a56b571ee
Sarosi Rares-Adrian <raressarosi@gmail.com>
2026-04-28 16:30:47 +0300

    gitignore

--- frontend/obj/Debug/net10.0/MediTap.Front.AssemblyInfo.cs
+++ frontend/obj/Debug/net10.0/MediTap.Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediTap.Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94466b78c04afa47398910c00b1ab87da72f5fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8b0113d0d9202b28ef0162da9fc3fb300ee295")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediTap.Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediTap.Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
